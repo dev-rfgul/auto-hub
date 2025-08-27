640225e85d99d21508885aa75d8705a4dfa5ffa0
dev-rfgul <rfgul587@gmail.com>
2025-08-27 08:54:49 +0500

    made routes and controllers for registration and login

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,19 @@
+import User from '../models/user.model.js';
+
+export const registerUser = async (req, res) => {
+  const { username, email, password, firstName, lastName, phone, address } = req.body;
+  const user = new User({ username, email, password, firstName, lastName, phone, address });
+  await user.save();
+  res.status(201).json(user);
+};
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+};
