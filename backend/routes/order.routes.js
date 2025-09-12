bc9b9d9909cbe668b815271a47585530c75583cf
dev-rfgul <rfgul587@gmail.com>
2025-09-12 09:45:06 +0500

    the order status is being updated now

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
-import { getOrder, getOrdersByUserId, getAllOrders,getOrdersByStoreId } from '../controllers/order.controller.js';
+import { getOrder, getOrdersByUserId, getAllOrders,getOrdersByStoreId, updateOrderStatus } from '../controllers/order.controller.js';
 
 router.get('/getOrderById/:id', getOrder);
 router.get('/getOrderByUserId/:userId', getOrdersByUserId);
 router.get('/getAllOrders', getAllOrders);
 router.get('/getOrdersByStoreId/:storeId', getOrdersByStoreId);
+router.patch('/updateOrderStatus/:id', updateOrderStatus);
 export default router;
